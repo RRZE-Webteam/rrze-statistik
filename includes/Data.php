41f0cfd801b6dd9c1f6bcb0415e34ed6f4ffd1d6
Lukas Niebler <nieblerlukas@gmail.com>
2022-02-11 14:13:46 +0100

    Arrayakrobatik Part 2

--- includes/Data.php
+++ includes/Data.php
@@ -17,7 +17,16 @@ class Data
 
     public function fetchLast24Months()
     {
-        $data = wp_remote_get('https://statistiken.rrze.fau.de/webauftritte/logs/www.wordpress.rrze.fau.de/webalizer.hist');
-        var_dump($data);
-    }
+        $data = wp_remote_get( esc_url_raw('https://statistiken.rrze.fau.de/webauftritte/logs/www.wordpress.rrze.fau.de/webalizer.hist'));
+        $data_body = wp_remote_retrieve_body($data);
+        $data_trim = rtrim($data_body," \n\r\t\v");
+        //$replaced = str_replace(" ", ",", $data_body);
+        //$output = preg_split("/ /", $data_body);
+        $array = preg_split("/\r\n|\n|\r/", $data_trim);
+        $output = [];
+        foreach($array as $value){
+            array_push($output, preg_split("/ /", $value));
+        }
+        var_dump($output);
+    } 
 }
