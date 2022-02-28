b09a8b20e72b0fa2572e90bff65864cc45c77d97
Lukas Niebler <nieblerlukas@gmail.com>
2022-02-28 09:41:19 +0100

    CamelCase entfernt und Autoformatter

--- includes/Analytics.php
+++ includes/Analytics.php
@@ -15,18 +15,20 @@ class Analytics
         $this->highcharts = new Highcharts($this->plugin_basename);
     }
 
-    public function getImgLink($type){
-        if ($type === 'forbidden'){
-            return plugin_dir_url(__DIR__).'assets/img/no_connection.svg';
+    public function getImgLink($type)
+    {
+        if ($type === 'forbidden') {
+            return plugin_dir_url(__DIR__) . 'assets/img/no_connection.svg';
         } else {
-            return plugin_dir_url(__DIR__).'assets/img/no_data.svg';
+            return plugin_dir_url(__DIR__) . 'assets/img/no_data.svg';
         }
     }
 
-    public function retrieveSiteUrl($debug){
-        if($debug === false){
-            $removeChar = ["https://", "http://", "/"];
-            return 'www.'.str_replace($removeChar, "", get_site_url());
+    public function retrieveSiteUrl($debug)
+    {
+        if ($debug === false) {
+            $remove_char = ["https://", "http://", "/"];
+            return 'www.' . str_replace($remove_char, "", get_site_url());
         } else {
             return "www.fau.de";
         }
@@ -37,11 +39,11 @@ class Analytics
         //set value of $url to true while debugging..
         $url = $this->retrieveSiteUrl(false);
         $data = new Data($this->plugin_basename);
-        $ready_check = $data->fetchLast24Months('https://statistiken.rrze.fau.de/webauftritte/logs/'.$url.'/webalizer.hist');
-        if ($ready_check === 'forbidden'){
-            return '<img src="'.$this->getImgLink('forbidden').'" alt=""><strong>Sie sind aktuell nicht mit dem Universitätsnetzwerk verbunden.</strong><br/>Verbinden Sie sich via VPN, um auf die letzten Statistiken zuzugreifen.';
-        } else if ($ready_check === 'no_data'){
-            return '<img src="'.$this->getImgLink('').'" alt=""><strong>Ihre Seite konnte nicht auf statistiken.rrze.fau.de gefunden werden.</strong><br/> Falls Ihre Seite ('.$url.') neu ist, kann es einen Monat dauern, bevor Statistiken dargestellt werden können.';
+        $ready_check = $data->fetchLast24Months('https://statistiken.rrze.fau.de/webauftritte/logs/' . $url . '/webalizer.hist');
+        if ($ready_check === 'forbidden') {
+            return '<img src="' . $this->getImgLink('forbidden') . '" alt=""><strong>Sie sind aktuell nicht mit dem Universitätsnetzwerk verbunden.</strong><br/>Verbinden Sie sich via VPN, um auf die letzten Statistiken zuzugreifen.';
+        } else if ($ready_check === 'no_data') {
+            return '<img src="' . $this->getImgLink('') . '" alt=""><strong>Ihre Seite konnte nicht auf statistiken.rrze.fau.de gefunden werden.</strong><br/> Falls Ihre Seite (' . $url . ') neu ist, kann es einen Monat dauern, bevor Statistiken dargestellt werden können.';
         } else {
             return $this->highcharts->lineplot();
         };
--- includes/Dashboard.php
+++ includes/Dashboard.php
@@ -6,19 +6,22 @@ defined('ABSPATH') || exit;
 
 class Dashboard
 {
-    public function __construct($plugin_basename){
+    public function __construct($plugin_basename)
+    {
         $this->plugin_basename = $plugin_basename;
         add_action('wp_dashboard_setup', [$this, 'my_custom_dashboard_widgets']);
     }
 
-    public function my_custom_dashboard_widgets() {
+    public function my_custom_dashboard_widgets()
+    {
         global $wp_meta_boxes;
-        
+
         wp_add_dashboard_widget('custom_help_widget', 'Seitenbesucher der letzten 24 Monate', [$this, 'custom_dashboard_help'], 'column3');
     }
-        
-    function custom_dashboard_help() {
+
+    function custom_dashboard_help()
+    {
         $analytics = new Analytics($this->plugin_basename);
-        echo( $analytics->getLinechart(get_site_url()) );
+        echo ($analytics->getLinechart(get_site_url()));
     }
 }
