2c793784a7060923bf8153991211e46048437c7d
rvdforst <rolf.v.d.forst@fau.de>
2022-03-01 11:21:33 +0100

    Update Highcharts.php

--- includes/Highcharts.php
+++ includes/Highcharts.php
@@ -9,26 +9,55 @@ defined('ABSPATH') || exit;
  */
 class Highcharts
 {
-    public function __construct($plugin_basename)
+    public function __construct()
     {
-        $this->plugin_basename = $plugin_basename;
         $this->loadHighcharts();
     }
 
     public function statistik_enqueue_script()
     {
-        wp_enqueue_style('highcharts-css', plugins_url('/assets/css/highcharts-style.css', $this->plugin_basename), array(), false, 'all');
-        wp_enqueue_script('highcharts-js', plugins_url('/assets/js/highcharts/9.3.3/highcharts.js', $this->plugin_basename), array('jquery'), false, true);
-        wp_enqueue_script('index-js', plugins_url('/assets/js/index.js', $this->plugin_basename), array('jquery'), false, true);
+        wp_enqueue_style(
+            'highcharts-css', 
+            plugins_url('assets/css/highcharts-style.css', plugin()->getBasename()),
+            array(), 
+            plugin()->getVersion(), 
+            'all'
+        );
+        wp_enqueue_script(
+            'highcharts-js', 
+            plugins_url('assets/js/highcharts/9.3.3/highcharts.js', plugin()->getBasename()),
+            array('jquery'), 
+            plugin()->getVersion(),
+            true
+        );
+        wp_enqueue_script(
+            'index-js', 
+            plugins_url('assets/js/index.js', plugin()->getBasename()),
+            array('jquery'), 
+            plugin()->getVersion(),
+            true
+        );
 
         $modules = ['accessibility', 'data', 'export-data', 'exporting'];
         foreach ($modules as $val) {
-            wp_enqueue_script('highcharts-module-' . $val, plugins_url('/assets/js/highcharts/9.3.3/modules/' . $val . '.js', $this->plugin_basename), array('jquery'), false, true);
+            wp_enqueue_script(
+                'highcharts-module-' . $val, 
+                plugins_url('assets/js/highcharts/9.3.3/modules/' . $val . '.js', plugin()->getBasename()),
+                array('jquery'), 
+                plugin()->getVersion(), 
+                true
+            );
         }
 
         $maps = ['accessibility', 'data', 'export-data', 'exporting'];
         foreach ($maps as $val) {
-            wp_enqueue_script('highcharts-module-' . $val, plugins_url('/assets/js/highcharts/9.3.3/modules/' . $val . '.js.map', $this->plugin_basename), array('jquery'), false, true);
+            wp_enqueue_script(
+                'highcharts-module-' . $val, 
+                plugins_url('assets/js/highcharts/9.3.3/modules/' . $val . '.js.map', plugin()->getBasename()),
+                array('jquery'), 
+                plugin()->getVersion(),
+                true
+            );
         }
     }
 
